bd969a5c54d31b240cb1f74858d04ff533363a2b
Anastasia Hristian <an.hristian@gmail.com>
2020-10-04 21:30:13 -0600

    WhileLoop - tricky; added WhileLoop that doesn't execute for confusion.

--- src/edu/cnm/deepdive/WhileLoop.java
+++ src/edu/cnm/deepdive/WhileLoop.java
@@ -27,6 +27,23 @@ public class WhileLoop {
       // 1. iteration: y=13, x=10; //it stops at x=10, because condition is x<10
     }
     System.out.println("x= " + x + " y= " + y);
+
+    int a = 5;
+    int b = 7;
+
+    //this while is here just to confuse us, because a is < 6 whileLoop will not execute
+    while (a > 6) {
+      b += 2;
+      a++;
+    }
+
+    a = a > 6 ? a++ : b--;
+    //false
+    // a = b -> a = 7;
+    // b-- -> b = 6
+    // a+b = 7 + 6 = 13
+    
+    System.out.println(a + ", "+ b + ", " + (a + b));
   }
 
 }
