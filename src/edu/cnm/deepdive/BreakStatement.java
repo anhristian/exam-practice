8c3c7db30c26cb4e6e4e629771a92e74ac1c0e54
Anastasia Hristian <an.hristian@gmail.com>
2020-10-07 23:33:28 -0600

    BreakStatement in a for-each-loop and while loop.

--- src/edu/cnm/deepdive/BreakStatement.java
+++ src/edu/cnm/deepdive/BreakStatement.java
@@ -3,16 +3,58 @@ package edu.cnm.deepdive;
 public class BreakStatement {
 
   public static void main(String[] args) {
-  //  myLabel:
-  //  int[] myArray = {1, 2, 3}; // not a statement for label
+    //  myLabel:
+    //  int[] myArray = {1, 2, 3}; // not a statement for label
 
     myLabel:
     {
-        int[] myArray = {1, 2, 3}; //but not a good practice to use like this, it is confusing
+      int[] myArray = {1, 2, 3}; //but not a good practice to use like this, it is confusing
     }
 
     String[] animals = {"Dog", "Cat", "Lizard", "Bird", "Snake"};
-    
+
+    for (String animal : animals) {
+      System.out.println(animal); // it prints the entire array with animals
+    }
+
+    MY_LOOP:
+    for (String animal : animals) {
+      if (animal.equals("Cat")) {
+        break MY_LOOP; // could be used just break.
+      }
+
+      System.out.println(animal);
+    }
+
+    // used animal as label to create confusion in the example.
+    animal:
+    for (String animal : animals) {
+      if (animal.equals("Cat")) {
+        break animal;
+      }
+    }
+
+    for (String animal : animals) {
+      if (animal.equals("Cat")) {
+        break;
+      }
+
+      System.out.println(animal);
+    }
+
+    // using break in while loop.
+    int index = 0;
+    while (index < animals.length) {
+      String animal = animals[index];
+
+      if (animal.equals("Lizard")) {
+        break;
+      }
+
+      System.out.println(animal);
+      index++;
+    }
+
   }
 
 }
