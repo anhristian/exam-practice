1f05813ad0d60c499401a74490618f7aa92a5e27
Anastasia Hristian <an.hristian@gmail.com>
2020-10-11 22:53:31 -0600

    working with constructors.

---
+++ src/edu/cnm/deepdive/ClassConstructors.java
@@ -0,0 +1,48 @@
+package edu.cnm.deepdive;
+
+class Dog {
+
+  private String color;
+
+  public Dog(String color) {   // this is a constructor
+    System.out.println("constructor");
+//    color = color; // here color refer to the parameter
+    this.color = color; // using this.color, it refers to the class field
+  }
+
+  public void printColor() {
+    System.out.println("color= " + color);
+  }
+
+  // public dog() {} // does not compile
+  // public void Dog(){} //this is not a constructor because it has return type void
+}
+
+class Cat {
+
+  private String color;
+  private int height;
+  private int length;
+
+  public Cat(int length, int theHeight) {
+// length = this.length;
+    this.length = length;
+    height = theHeight;
+    color = "white";
+  }
+
+  public void printInfo() {
+    System.out.println("Cat length= " + length + " height= " + height + " color= " + color);
+  }
+}
+
+public class ClassConstructors {
+
+  public static void main(String[] args) {
+    Dog dog = new Dog("black");  //calling the constructor here
+    dog.printColor();
+
+    Cat cat = new Cat(10, 15);
+    cat.printInfo();
+  }
+}
