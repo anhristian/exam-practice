7719880943530b8b2061697f2ecf0358a7a7edaf
Anastasia Hristian <an.hristian@gmail.com>
2020-10-12 23:27:11 -0600

    constructors chain.

--- src/edu/cnm/deepdive/constructor/DefaultConstructor.java
+++
@@ -1,9 +0,0 @@
-package edu.cnm.deepdive.constructor;
-
-public class DefaultConstructor {
-
-  public static void main(String[] args) {
-    ExampleWithDog exampleWithDog = new ExampleWithDog();
-  }
-
-}
--- src/edu/cnm/deepdive/constructor/ExampleWithDog.java
+++
@@ -1,5 +0,0 @@
-package edu.cnm.deepdive.constructor;
-
-public class ExampleWithDog {
-
-}
