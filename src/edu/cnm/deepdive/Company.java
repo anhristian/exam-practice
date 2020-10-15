d2f258b50a54e70c8954388e084438d84a97759a
Anastasia Hristian <an.hristian@gmail.com>
2020-10-14 23:25:54 -0600

    DataEncapsulation example.

--- src/edu/cnm/deepdive/Company.java
+++ src/edu/cnm/deepdive/Company.java
@@ -4,17 +4,38 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+// POJO (Plain Old Java Object)
 public class Company {
 
-  public String name;
-  public List<String> employees = new ArrayList<>();
+  private String name;
+  private List<String> employees = new ArrayList<>();
 
   // public Company() {} //default constructor
 
-  public void printSorted(){
+  public void printSorted() {
     System.out.println("companyName= " + name);
     Collections.sort(employees);
     System.out.println("sorted= " + employees);
+  }
+
+  public void setName(String name) {
+    if (name == null) {
+      System.out.println("name cannot be null");
+    } else {
+      this.name = name;
+    }
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void addEmployee(String name) {
+    if (name == null) {
+      System.out.println("cannot add null employee");
+    } else {
+      employees.add(name);
+    }
   }
 
 }
--- src/edu/cnm/deepdive/DataEncapsulation.java
+++ src/edu/cnm/deepdive/DataEncapsulation.java
@@ -4,13 +4,19 @@ public class DataEncapsulation {
 
   public static void main(String[] args) {
     Company company = new Company();
-    company.name = "MyCompany";
-    company.employees.add("John");
-    company.employees.add("Anthony");
+    company.setName("MyCompany");
+    company.addEmployee("John");
+    company.addEmployee("Anthony");
+    company.addEmployee(null); // get point null exception, cannot sort list with null
+
+    company.setName(null);
 
     company.printSorted();
 
-    company.employees = null;
+   // company.employees = null;
+
+   //  company.employees.add("Jimmy");
+  //  company.printSorted();
   }
 
 }
