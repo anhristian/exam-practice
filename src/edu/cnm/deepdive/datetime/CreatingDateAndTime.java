ee4dd63acb324f5e0921185034e6be778407737a
Anastasia Hristian <an.hristian@gmail.com>
2020-10-20 21:46:13 -0600

    Current Date and Time.

--- src/edu/cnm/deepdive/datetime/CreatingDateAndTime.java
+++ src/edu/cnm/deepdive/datetime/CreatingDateAndTime.java
@@ -1,9 +1,23 @@
 package edu.cnm.deepdive.datetime;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
 public class CreatingDateAndTime {
 
   public static void main(String[] args) {
-    
+
+    System.out.println(LocalDate.now());
+    System.out.println(LocalDateTime.now());
+    System.out.println(LocalTime.now());
+
+    LocalDate localDate = LocalDate.of(2020, Month.OCTOBER, 20);
+    LocalTime localTime = LocalTime.of(10,30);
+    System.out.println(localDate);
+    System.out.println(localTime);
+    System.out.println(LocalDateTime.of(localDate, localTime));
   }
 
 }
