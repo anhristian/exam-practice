5231135fe1d7400dcde58d68f56be4c6b2c242ac
Anastasia Hristian <an.hristian@gmail.com>
2020-10-24 23:34:15 -0600

    recognizing exceptions hierarchy 2.

--- src/edu/cnm/deepdive/exception/CatchingExceptions.java
+++ src/edu/cnm/deepdive/exception/CatchingExceptions.java
@@ -13,11 +13,34 @@ public class CatchingExceptions {
   public static void main(String[] args) {
     try {
       visitMuseum();
-    } catch (MuseumClosedForLunch mcl) { //subclass
+    } catch (MuseumClosedForLunch mcl) { //superclass
       System.out.println("Closed for lunch, come back in 2 hours");
     } catch (MuseumClosed mc) { //subclass
       System.out.println("Closed");
     }
+    System.out.println(calculate());
+  }
+
+  public static String calculate() {
+    String result = "";
+    String str = null;
+
+    try {
+      try {
+        result += "start"; //result= start
+        str.length();
+        result += "end"; //not execute
+      } catch (NullPointerException e) {
+        result +="npe"; // result = startnpe
+        throw new RuntimeException();
+      } finally {
+        result += "finally"; //result = startnpefinally
+        throw new Exception();
+      }
+    } catch (Exception e) { //gets executed because finally block trows exception
+      result += "finished"; // result = startnpefinallyfinished
+    }
+    return result;
   }
 
   public static void visitMuseum() {
