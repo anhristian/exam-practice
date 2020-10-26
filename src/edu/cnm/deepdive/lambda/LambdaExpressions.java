5dbeabe484df56b08569138bfa2f224d365c9424
Anastasia Hristian <an.hristian@gmail.com>
2020-10-25 22:38:32 -0600

    simple lambda.

---
+++ src/edu/cnm/deepdive/lambda/LambdaExpressions.java
@@ -0,0 +1,65 @@
+package edu.cnm.deepdive.lambda;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Animal {
+
+  private String type;
+  private boolean canJump;
+  private boolean canSwim;
+
+  public Animal(String type, boolean canJump, boolean canSwim) {
+    this.type = type;
+    this.canJump = canJump;
+    this.canSwim = canSwim;
+  }
+
+  public boolean canJump() {
+    return canJump;
+  }
+
+  public boolean isCanSwim() {
+    return canSwim;
+  }
+
+  public String getType() {
+    return type;
+  }
+}
+    interface CheckAnimal {
+      boolean check (Animal animal);
+    }
+    class CheckCanJump implements CheckAnimal {
+
+      @Override
+      public boolean check(Animal animal) {
+        return animal.canJump();
+      }
+    }
+
+public class LambdaExpressions {
+
+  public static void main(String[] args) {
+    List<Animal> animals = new ArrayList<>();
+    animals.add(new Animal("fish", false, true ));
+    animals.add(new Animal("rabbit", true, false));
+    animals.add(new Animal("dog", true, true));
+
+    print(animals, new CheckCanJump());
+
+    print(animals, animal -> animal.canJump());
+    print(animals, animal -> !animal.canJump());
+
+  }
+
+  private static void print(List<Animal> animals, CheckAnimal filter) {
+    for (Animal animal : animals) {
+      if (filter.check(animal)) {
+        System.out.println(animal.getType());
+      }
+    }
+    System.out.println();
+  }
+}
